f8ba03a9a1268caf6b15c0ec5314e35d7976b83f
Hugo Alliaume <kocal@live.fr>
2017-01-28 17:14:57 +0100

    Now using Redis for the cache

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,16 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Tag;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 
 class ApiController extends Controller
 {
-    public function getTags()
+    public function getSortedTags()
     {
-        $json = Cache::remember('tags', 10, function () {
-            $tags = Tag::select(['tag', 'slug'])->orderBy('tag')->get();
-            return json_encode(['tags' => $tags]);
-        });
+        $sortedTags = Redis::get('sortedTags');
 
-        return $json;
+        if (!$sortedTags) {
+            $sortedTags = $this->fetchShortedTags();
+            Redis::set('sortedTags', $sortedTags);
+        }
+
+        return $sortedTags;
+    }
+
+    private function fetchShortedTags()
+    {
+        $sortedTags = [];
+        $tags = Tag::select(['tag', 'slug'])->orderBy('tag')->get();
+
+        foreach ($tags as $tag) {
+            $key = $tag->slug[0];
+            if (is_numeric($key)) $key = '0-9';
+
+            $sortedTags[$key][] = $tag;
+        }
+
+        return json_encode(compact('sortedTags'));
     }
 }
