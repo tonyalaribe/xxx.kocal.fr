02fb04a53c5516183f6a3df45f9e9c5a643445de
Hugo Alliaume <kocal@live.fr>
2017-03-11 17:42:39 +0100

    La page "tags" affiche bien la liste de tous les tags

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -2,15 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Tag;
+use Illuminate\Support\Facades\Cache;
+
 class TagsController extends Controller
 {
     public function showTagsAction()
     {
-        return view('tags');
+        $sortedTags = Cache::remember('sortedTags', 60 * 60 * 24, function () {
+            return $this->fetchTags();
+        });
+
+        return view('tags', [
+            'sortedTags' => $sortedTags
+        ]);
     }
 
     public function showTagAction()
     {
 
+    }
+
+    private function fetchTags()
+    {
+        $sortedTags = [];
+        $tags = Tag::select(['tag', 'slug'])->orderBy('tag')->get();
+
+        foreach ($tags as $tag) {
+            $key = $tag->slug[0];
+            if (is_numeric($key)) {
+                $key = '0-9';
+            }
+
+            if ($tag->slug === $tag->tag) {
+                continue;
+            }
+
+            $sortedTags[$key][] = $tag;
+        }
+
+        return $sortedTags;
     }
 }
--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -4,5 +4,16 @@
 @section('description', 'List of all tags on xxx.kocal.fr')
 
 @section('body')
-    <h1>/tags</h1>
+    <h1>All tags</h1>
+
+    @foreach($sortedTags as $key => $tags)
+        <h2>{{$key}}</h2>
+        <ul class="list-tags">
+            @foreach($tags as $tag)
+                <li>
+                    <a href="{{ route('tag', ['tag' => $tag->slug]) }}">{{$tag->tag}}</a>
+                </li>
+            @endforeach
+        </ul>
+    @endforeach
 @endsection
